d9a32b927bf06982076d233a981b6af805797b0c
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-03-16 20:34:27 +0200

    creating the second VC

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello, World")
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+   
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording pressed")
+        recordingLabel.text = "Tap to start recording"
+    }
+    
 }
 
