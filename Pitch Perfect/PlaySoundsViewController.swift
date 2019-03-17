8eca43fba430da3fec3adead490d3249d2f2fea3
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-03-17 20:51:37 +0200

    Adding Buttons in 2nd VC

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Ahmed Afifi on 3/17/19.
+//  Copyright © 2019 Ahmed Afifi. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    
+    var recordedAudioUrl: URL!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
