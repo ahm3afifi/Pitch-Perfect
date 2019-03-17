5470e7d0fc044d607fca7851575e170664c63a6d
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-03-17 14:27:04 +0200

    Adding AVAudioRecorder

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -8,25 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController {
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var stopRecordingButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("Hello, World")
+        stopRecordingButton.isEnabled = false
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button was pressed")
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
         recordingLabel.text = "Recording in Progress"
     }
    
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop recording pressed")
-        recordingLabel.text = "Tap to start recording"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+        
     }
     
 }
